d0acfca5036a3d9814adb49a8d059be647ae6614
berkay <berkayoruc@hotmail.com>
2023-03-06 21:27:39 +0300

    second paragraph

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -12,9 +12,10 @@ const Content = () => {
         uygulamalar geliştiriyorum.
       </ContentParagraph>
       <ContentParagraph>
-        Kodlamayı seven bir harita mühendisiyim. Profesyonel olarak web frontend
-        geliştiricisi olarak çalışsam da, Android ve iOS ekosistemi için de
-        uygulamalar geliştiriyorum.
+        İnsanların hayatına dokunan uygulamalar geliştirmeyi, farklı
+        teknolojileri deneyimlemeyi seviyorum. İlgi alanlarımın başında futbol
+        ve Formula 1 geliyor. Ancak herhangi bir spor müsabakasını soluksuz
+        izleyebilirim 🏅
       </ContentParagraph>
       <ContentParagraph>
         Hakkımda daha fazla bilgiye ihtiyaç duyarsan, bana aşağıdaki platformlar
